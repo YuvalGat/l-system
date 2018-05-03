eaa77f4ca0da62cb32930bd4d5a60af41cf5b777
Yuval Gat <yuvalgat11@gmail.com>
2018-05-03 20:52:16 +0300

    Change colors and add stars

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
 	createCanvas(1000, 1000);
-	// noLoop();
 	background(0);
 	stroke(255);
 	strokeWeight(2);
 	frameRate(60);
+	colorMode(HSB, 360, 1, 1);
 }
 
 function draw() {
@@ -22,23 +22,34 @@ function draw() {
 	const sys = new LSystem(axiom, rules);
 	const pattern = sys.iterate(7);
 
-	for (const char of pattern) {
+	for (let i = 0; i < pattern.length; i++) {
+		const char = pattern[i];
+
 		switch (char) {
 			case '0':
+				stroke((i / pattern.length * 50 + 300) % 360, 1, 1);
+				line(0, 0, 0, -length);
+				translate(0, -length);
+				break;
 			case '1':
+				stroke(255);
 				line(0, 0, 0, -length);
 				translate(0, -length);
 				break;
 			case '[':
 				push();
-				rotate((sin(frameCount / 30) + 1) * PI / 8);
+				rotate((sin(frameCount / 25) + 1) * PI / 8);
 				break;
 			case ']':
 				pop();
-				rotate(-(sin(frameCount / 40) + 1) * PI / 8);
+				rotate(-(sin(frameCount / 33) + 1) * PI / 8);
 				break;
 			default:
 				break;
+		}
+
+		if (random() < 0.005) {
+			point(random(width) - width / 2, random(height));
 		}
 	}
 }
